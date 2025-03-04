7dd3a213932f2884ed264ce3358ac16fa1c23c22
TH-Raju <rajukhan894200@gmail.com>
2025-03-04 11:35:11 +0600

    [Condition] in go

--- first-program/main.go
+++ first-program/main.go
@@ -16,6 +16,14 @@ func main(){
 
 
 	fmt.Println("Hello on Go in", b,"st Ramadan!");
+
+
+	age := 21
+	if age >= 21 {
+		fmt.Println("You are eligible to drive!")
+	}else if age >= 18 {
+		fmt.Println("You are not eligible to drive!")
+	}
 }
 
 
