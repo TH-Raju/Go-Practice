51f3ed1e3dac0d3d26379c78feaf41869e6e5712
TH-Raju <rajukhan894200@gmail.com>
2025-03-04 11:49:43 +0600

    [switch] in go

--- first-program/main.go
+++ first-program/main.go
@@ -18,11 +18,25 @@ func main(){
 	fmt.Println("Hello on Go in", b,"st Ramadan!");
 
 
-	age := 21
+	age := 2
 	if age >= 21 {
 		fmt.Println("You are eligible to drive!")
 	}else if age >= 18 {
 		fmt.Println("You are not eligible to drive!")
+	}else {
+		fmt.Println("You are not old enough to drive!")
+	}
+
+
+	x := 3
+
+	switch x {
+		case 1:
+            fmt.Println("One")
+        case 2, 3:
+            fmt.Println("Two or Three")
+        default:
+            fmt.Println("Unknown")
 	}
 }
 
