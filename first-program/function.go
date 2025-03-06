0c88fb7dafac267d20569649e18f9541969d5cc2
TH-Raju <rajukhan894200@gmail.com>
2025-03-06 11:50:34 +0600

    [Multi value return] in go

--- first-program/function.go
+++ first-program/function.go
@@ -8,6 +8,14 @@ func add(num1 int, num2 int) int{
 	return sum
 }
 
+func getNumbers(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	mul := num1 * num2
+
+	return sum, mul
+
+}
+
 func main(){
 
 	a := 10
@@ -19,4 +27,7 @@ func main(){
 	summation := add(a, b)
 
 	fmt.Println("Sum with function: ", summation)
+
+	p, q := getNumbers(a, b)
+	fmt.Println("p: ", p, " q: ", q)
 }
