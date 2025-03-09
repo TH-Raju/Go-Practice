b09216b815c10393b978655037fdf1db5b4e5fb9
TH-Raju <rajukhan894200@gmail.com>
2025-03-09 11:24:58 +0600

    [re-usable function] in go

--- first-program/input.go
+++ first-program/input.go
@@ -3,31 +3,55 @@ package main
 import "fmt"
 
 
-func main(){
-	fmt.Println("Starting")
+func printWelcomeMessage(){
+	fmt.Println("Welcome to the Application")
+}
 
+func getUserName() string {
 	var name string
 	fmt.Println("Enter your name: ")
 	fmt.Scanln(&name)
+	return name
+}
 
-	fmt.Printf("Hello, %s", name)
-
-	var num1 int
-	var num2 int
+func getTwoNumber() (int, int){
 	fmt.Println("Enter first number: ")
-	fmt.Scanln(&num1)
-	fmt.Println("Enter second number: ")
-	fmt.Scanln(&num2)
+    var num1 int
+    fmt.Scanln(&num1)
 
+    fmt.Println("Enter second number: ")
+    var num2 int
+    fmt.Scanln(&num2)
+
+    return num1, num2  
+}
+
+func add(num1 int, num2 int) int {
 	sum := num1 + num2
+	return sum
+}
 
-	// Display results
-
+func displayResults(name string, sum int){
 	fmt.Println("Hello, ", name)
 	fmt.Println("Summation: ", sum)
+}
 
-	// Goodbye message
-
-	fmt.Println("Thank your for your Information")
+func getGoodbyeMessage(name string) {
+    fmt.Println("Thank your for your Information")
 	fmt.Println("Goodbye, ", name)
+}
+
+func main(){
+	printWelcomeMessage()
+	name := getUserName()
+
+	num1, num2 := getTwoNumber()
+	sum := add(num1, num2)
+	
+	// Display results
+	displayResults(name, sum)
+	
+	// Goodbye message
+	getGoodbyeMessage(name)
+	
 }
