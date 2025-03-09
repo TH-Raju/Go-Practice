ea37d0fed5f0fdd94868b59804119c5c98b4c062
TH-Raju <rajukhan894200@gmail.com>
2025-03-09 11:14:41 +0600

    [Input output] in go

--- first-program/input.go
+++ first-program/input.go
@@ -11,4 +11,23 @@ func main(){
 	fmt.Scanln(&name)
 
 	fmt.Printf("Hello, %s", name)
+
+	var num1 int
+	var num2 int
+	fmt.Println("Enter first number: ")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter second number: ")
+	fmt.Scanln(&num2)
+
+	sum := num1 + num2
+
+	// Display results
+
+	fmt.Println("Hello, ", name)
+	fmt.Println("Summation: ", sum)
+
+	// Goodbye message
+
+	fmt.Println("Thank your for your Information")
+	fmt.Println("Goodbye, ", name)
 }
