b7a12e10d56955f18b3c71e7cec7ed2fc1f52bc7
TH-Raju <rajukhan894200@gmail.com>
2025-03-15 09:54:51 +0600

    [Package Practice] in go

--- first-program/packageScope.go
+++ first-program/packageScope.go
@@ -1,10 +1,12 @@
 package main
 import ("fmt" 
-"example.com/mathlib")
+"example.com/mathlib"
+"example.com/test")
 
 func main(){
 	fmt.Println("Learn Package Scope!!")
 	mathlib.Add(4,6)
+	test.Hello()
 }
 
 // go mod init example.com  
