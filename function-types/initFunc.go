5cc0ca1eecd450535e79e0ad68ecffe6ea84b839
TH-Raju <rajukhan894200@gmail.com>
2025-03-18 10:52:54 +0600

    [Init function complete] in Go

--- function-types/initFunc.go
+++ function-types/initFunc.go
@@ -1,6 +1,8 @@
 package main
 import "fmt"
 
+
+var a = 10
 func main(){
 	// var name string
 	// fmt.Print("Enter your name: ")
@@ -8,8 +10,13 @@ func main(){
 
 	// fmt.Println("Hello, ", name)
 
+	fmt.Println("Hello start Init Function")
+	fmt.Println(a)
+
 }
 
 func init(){
 	fmt.Println("I am the first function that execute first...")
+	fmt.Println("From init",a)
+	a = 20
 }
