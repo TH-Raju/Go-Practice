019315ab10957f590548251c6d2e1f396ddf03fd
TH-Raju <rajukhan894200@gmail.com>
2025-03-25 14:42:18 +0600

    [Quiz Project] added title

--- practice-project/quiz.go
+++ practice-project/quiz.go
@@ -3,5 +3,22 @@ import "fmt"
 
 
 func main(){
-	fmt.Println("Hello From Quiz test")
+	var name string
+	var ans string
+	fmt.Println("Hello!!")
+	fmt.Print("May I know your name? : ")
+	fmt.Scan(&name)
+	fmt.Println("Hello!", name, "Are you ready to start the Quiz......!!!")
+	fmt.Print("Enter (Y) for yes and (N) for no: ")
+	fmt.Scan(&ans)
+
+	if(ans == "Y" || ans == "y"){
+		fmt.Println("Let's begin the quiz!")
+	}else {
+		fmt.Println("Okay, Quiz is over!")
+        return
+	}
+
+
+
 }
