fd894d50f1d5afaba1ae01edd9e890e92b4a1d22
TH-Raju <rajukhan894200@gmail.com>
2025-03-26 11:35:16 +0600

    [Question add] in Golang Quiz project

--- practice-project/quiz.go
+++ practice-project/quiz.go
@@ -5,6 +5,7 @@ import "fmt"
 func main(){
 	var name string
 	var ans string
+	var score int
 	fmt.Println("Hello!!")
 	fmt.Print("May I know your name? : ")
 	fmt.Scan(&name)
@@ -14,6 +15,28 @@ func main(){
 
 	if(ans == "Y" || ans == "y"){
 		fmt.Println("Let's begin the quiz!")
+		fmt.Println("Q1: 1 + 1 = ? ")
+		var userAns int
+		fmt.Print("Write Answer:")
+		fmt.Scan(&userAns)
+		if(userAns == 2){
+			score++
+            fmt.Println("Correct Answer!")
+        }else {
+            fmt.Println("Wrong Answer!")
+        }
+
+		fmt.Println("Q2: 2 * 3 =? ")
+        fmt.Print("Write Answer:")
+        fmt.Scan(&userAns)
+        if(userAns == 6){
+            score++
+            fmt.Println("Correct Answer!")
+        } else {
+            fmt.Println("Wrong Answer!")
+        }
+
+		fmt.Println("Your Score : ", score)
 	}else {
 		fmt.Println("Okay, Quiz is over!")
         return
